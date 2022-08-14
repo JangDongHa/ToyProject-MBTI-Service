559e5323769b26230057b05788c01a87469abf25
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2022-08-15 00:42:04 +0900

    s3Config 삭제(2022/08/15)

--- src/main/java/com/example/miniproject/controller/BoardApiController.java
+++ src/main/java/com/example/miniproject/controller/BoardApiController.java
@@ -1,9 +1,14 @@
 package com.example.miniproject.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class BoardApiController {
+    @GetMapping("/")
+    public String test(){
+        return "ok";
+    }
 }
--- src/main/java/com/example/miniproject/controller/UserApiController.java
+++ src/main/java/com/example/miniproject/controller/UserApiController.java
@@ -2,6 +2,7 @@ package com.example.miniproject.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
